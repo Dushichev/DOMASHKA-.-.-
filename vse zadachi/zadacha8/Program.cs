d99409a5eae0707eea5d5ad10e6cad9157fb0ea2
Dushichev <108294464+Dushichev@users.noreply.github.com>
2022-07-10 15:09:44 +0300

    решил задачу 8

---
+++ vse zadachi/zadacha8/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.
+*/
+Console.WriteLine("введите число A равное 1");
+int A  = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число N");
+int N  = int.Parse(Console.ReadLine());
+
+for(int i = A;i<=N;i++)
+{    
+    if(i%2==0)
+    Console.WriteLine(i);
+}
